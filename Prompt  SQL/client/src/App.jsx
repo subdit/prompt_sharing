ec9957e72ebae580c88708539261c08a9d200bdf
Kasidit Bualoeng <subdit@gmail.com>
2024-03-05 14:17:13 -0500

    created input form

--- Prompt  SQL/client/src/App.jsx
+++ Prompt  SQL/client/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from './index.module.css';
+import sqlLogo from './assets/sql-server.png';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className={styles.main}>
+      <img src={sqlLogo} alt='' className='styles.icon' />
+      <h3>Generate SQL with Chat GPT v3.5 AI</h3>
+
+      <form>
+        <input
+          type='text'
+          name='query-description'
+          placeholder='Describe Your SQL Query'
+        />
+        <input type='submit'>Generate Query</input>
+      </form>
+    </main>
+  );
 }
 
-export default App
+export default App;
